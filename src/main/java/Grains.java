10eb652e770eb19c7267a96687ada133582cd868
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-16 01:08:30 -0500

    Initial commit

--- src/main/java/Grains.java
+++ src/main/java/Grains.java
@@ -2,12 +2,24 @@ import java.math.BigInteger;
 
 class Grains {
 
-    BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    BigInteger grainsOnSquare(final int square) throws IllegalArgumentException {
+        
+        if (square <= 0 || square > 64) throw new IllegalArgumentException("square must be between 1 and 64");
+        
+        BigInteger result = BigInteger.ONE;
+        
+        for (int multiplier = 1; multiplier < square; multiplier++) {
+            result = result.multiply(BigInteger.TWO);
+        }
+        return result;
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        BigInteger result = BigInteger.ZERO;
+        for (int i = 1; i <= 64; i++) {
+            result = result.add(grainsOnSquare(i));
+        }
+        return result;
     }
 
 }
